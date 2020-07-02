cacb1ef38475ecde4a78bc8457e2a21f9b59f8c7
Vladimir <larikhin@gmail.com>
2020-07-02 15:13:07 +0300

    max4

--- l4/max2.py
+++ l4/max2.py
@@ -7,9 +7,12 @@ def max2(x,y):
 def max3(x,y,z):
     return max2(x,max2(y,z))
 
+def max4(x,y,z,k):
+    return max2(k,max3(x,y,z))
 
 x = int(input())
 y = int(input())
 z = int(input())
+k = int(input())
 
-print(max3(x,y,z))
+print(max4(x,y,z,k))
